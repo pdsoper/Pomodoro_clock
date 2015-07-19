82949b91afc742c7f8550d7832033eb772d1218c
soap2k6 <pdsoper@gmail.com>
2015-07-19 15:26:42 -0400

    Ready to start adding Javascript

--- scripts.js
+++ scripts.js
@@ -2,4 +2,19 @@
 
 $(document).ready(function() {
 
+/*
+Add controls for session and break length.  The minimum value is 1
+Update the value shown in the control
+When the session legnth changes, update the value in the timeer
+Disable the controls while the timer is running
+Click on the timer to start and pause it
+Start the break timer as soon as the session timer is done
+Start the session timer as soon as the break timer is done
+Play a sound when the break or session timer starts, except for the first
+time (i.e., when first loaded or after the session or break settings have
+been changed)
+Fill the timer according to the percentage of the time that has passed.  Update it when the timer is updated.
+Reset the fill and change the fill color when tranitioning from session to break
+*/
+
 });
